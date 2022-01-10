d4a88eff8a07152b10ec3a2c8ce045ff1c1f203d
uuhuang918 <uuhuang918@gmail.com>
2022-01-10 15:29:37 +0800

    add RemoveItem

--- Models/Entities/CartEntity.cs
+++ Models/Entities/CartEntity.cs
@@ -33,24 +33,42 @@ namespace NTUB.BookStore.Site.Models.Entities
 		public int Total => Items == null || Items.Count == 0 ? 0 : Items.Sum(x => x.SubTotal);
 		public  ShippingInfo ShippingInfo { get; set; }
 
-		public void AddItem(CartProductEntity product,int qty)
+		public void AddItem(CartProductEntity product, int qty)
 		{
-			if(product == null)throw new ArgumentNullException(nameof(product));
-			if(qty <= 0)throw new ArgumentOutOfRangeException(nameof(qty));
+			if (product == null) throw new ArgumentNullException(nameof(product));
+			if (qty <= 0) throw new ArgumentOutOfRangeException(nameof(qty));
 
-			var item=Items.SingleOrDefault(x => x.Product.Id ==product.Id);
-			if(item == null)
+			var item = Items.SingleOrDefault(x => x.Product.Id == product.Id);
+			if (item == null)
 			{
-				var cartItem=new CartItemEntity(product,qty);
+				var cartItem = new CartItemEntity(product, qty);
 				this.Items.Add(cartItem);
 			}
 			else
 			{
-				item.Qty = qty;
+				item.Qty += qty;
 			}
 		}
+		 public void RemoveItem(int productId)
+			{
+				var item = Items.SingleOrDefault(x => x.Product.Id == productId);
+			if (item == null) return;
+			Items.Remove(item);
+			}
+
+		 public void UpdateQty(int productId,int newQty)
+		{
+			if(newQty<=0)
+			{
+				RemoveItem(productId);
+				return;
+			}
+
+			var item = Items.SingleOrDefault(x=>x.Product.Id == productId);
+			item.Qty = newQty;
+		}
+	}
 
 
 
 	}
-}
