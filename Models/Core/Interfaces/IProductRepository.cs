4fff3e815c4758a888340c4f43e17d947ed96458
uuhuang918 <uuhuang918@gmail.com>
2022-01-10 10:14:25 +0800

    IProductRepository

---
+++ Models/Core/Interfaces/IProductRepository.cs
@@ -0,0 +1,30 @@
+﻿using NTUB.BookStore.Site.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NTUB.BookStore.Site.Models.Core.Interfaces
+{
+	public interface IProductRepository
+	{
+	/// <summary>
+	/// 
+	/// </summary>
+	/// <param name="categoryId"></param>
+	/// <param name="productName"></param>
+	/// <param name="status"></param>
+	/// <returns></returns>
+		IEnumerable<ProductEntity> Search(int? categoryId, string productName, bool? status);
+
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="productId"></param>
+		/// <param name="status"></param>
+		/// <returns></returns>
+		ProductEntity Load(int? productId, bool? status);
+	}
+}
