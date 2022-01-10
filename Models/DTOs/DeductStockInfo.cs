0815bc3ff714301ba2c5f3d2d795cb37accbe004
uuhuang918 <uuhuang918@gmail.com>
2022-01-10 10:51:43 +0800

    add ReviseStockInfo、DeductStockInfo

---
+++ Models/DTOs/DeductStockInfo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NTUB.BookStore.Site.Models.DTOs
+{
+	public class DeductStockInfo
+	{
+		/// <summary>
+		/// 新增訂單時，要扣除庫存量的資訊
+		/// </summary>
+		public int ProductId { get; set; }
+
+	/// <summary>
+	/// 傳入正數
+	/// </summary>
+		public int Qty { get; set; }
+	}
+}
---
+++ Models/DTOs/ReviseStockInfo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NTUB.BookStore.Site.Models.DTOs
+{
+	public class ReviseStockInfo
+	{
+		/// <summary>
+		/// 訂單取消時，要增加庫存量的資訊
+		/// </summary>
+		public int ProductId { get; set; }
+
+		/// <summary>
+		/// 傳入正數
+		/// </summary>
+		public int Qty { get; set; }
+	}
+}
