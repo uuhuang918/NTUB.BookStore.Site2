4e9b6bbf374ab945a130bb5a5fca552559905213
uuhuang918 <uuhuang918@gmail.com>
2022-01-10 10:29:38 +0800

    ProductService

---
+++ Models/Core/ProductService.cs
@@ -0,0 +1,23 @@
+﻿using NTUB.BookStore.Site.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NTUB.BookStore.Site.Models.Core.Interfaces
+{
+	public class ProductService
+	{
+		private readonly IProductRepository _repository;
+		public  ProductService(IProductRepository repository)
+		{
+			_repository = repository;
+		}
+
+		public IEnumerable<ProductEntity> Search(int?categoryId,string productName)
+			=>_repository.Search(categoryId,productName,true);
+
+		public ProductEntity Load(int producId)
+			=>_repository.Load(producId,true);
+	}
+}
