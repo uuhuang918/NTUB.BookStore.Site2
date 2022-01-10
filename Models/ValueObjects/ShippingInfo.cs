38916709746af22843ff21ce568498e09515de51
uuhuang918 <uuhuang918@gmail.com>
2022-01-10 14:22:27 +0800

    ShippingInfo

---
+++ Models/ValueObjects/ShippingInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NTUB.BookStore.Site.Models.ValueObjects
+{
+	public class ShippingInfo
+	{
+
+		public string Receiver { get; set; }
+		public string Address { get; set; }
+		public string CellPhone { get; set; }
+	}
+}
