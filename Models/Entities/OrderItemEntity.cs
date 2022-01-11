731fc4fbb31e3fdba435f02695466222681db0b0
uuhuang918 <uuhuang918@gmail.com>
2022-01-11 13:59:07 +0800

    add OrderItemEntity

---
+++ Models/Entities/OrderItemEntity.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NTUB.BookStore.Site.Models.Entities
+{
+	public class OrderItemEntity
+	{
+		public OrderItemEntity(int id,OrderProductEntity product,int qty)
+		{
+			Id = id;
+			Product = product;
+			Qty = qty;
+		}
+		public int Id { get; set; }
+		private OrderProductEntity _Product;
+		public OrderProductEntity Product
+		{ get => _Product;
+			set => _Product = value != null ? value : throw new Exception("Product不能是null");
+				}
+
+		private int _Qty;
+		public int Qty
+		{
+			get => _Qty;
+			set => _Qty = value > 0 ? value : throw new Exception("Qty必須大於0");
+
+		}
+
+		public int SubTotal=>Product.Price*Qty;
+
+	}
+}
