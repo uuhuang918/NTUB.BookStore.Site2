99e9bf4a25f7cfac53ddd475f0eb3d92a167ed8f
uuhuang918 <uuhuang918@gmail.com>
2022-01-13 16:56:49 +0800

    1/13

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,32 @@
+﻿using NTUB.BookStore.Site.Models.Core.Interfaces;
+using NTUB.BookStore.Site.Models.EFModels;
+using NTUB.BookStore.Site.Models.Infrastructures.Repositories;
+using NTUB.BookStore.Site.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NTUB.BookStore.Site.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private ProductService productService;
+
+
+        public ProductsController()
+		{
+            var db = new AppDbContext();
+            IProductRepository repo = new ProductRepository(db);
+            this.productService = new ProductService(repo);
+		}
+
+        // GET: Products
+        public ActionResult Index()
+        {
+            var data = productService.SearchActiveProducts(null, null).Select(x => x.ToVM());
+            return View(data);
+        }
+    }
+}
---
+++ Models/ViewModels/ProductVM.cs
@@ -0,0 +1,50 @@
+﻿using NTUB.BookStore.Site.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace NTUB.BookStore.Site.Models.ViewModels
+{
+	public class ProductVM
+	{
+        public int Id { get; set; }
+
+        [Display(Name="名稱")]
+        public string Name { get; set; }
+
+        [Display(Name = "分類名稱")]
+        public string CategoryName { get; set; }
+
+        [Display(Name = "商品描述")]
+        public string Description { get; set; }
+
+        [Display(Name = "售價")]
+        public int Price { get; set; }
+
+
+        [Display(Name = "商品照片")]
+        public string ProductImage { get; set; }
+
+        [Display(Name = "庫存量")]
+        public int Stock { get; set; }
+    }
+
+    public static partial class ProductEntityExts
+	{
+        public static ProductVM ToVM(this ProductEntity source)
+		{
+            return new ProductVM
+            {
+                Id = source.Id,
+                Name = source.Name,
+                CategoryName = source.Category.Name,
+                Description = source.Description,
+                Price = source.Price,
+                Stock = source.Stock,
+                ProductImage = source.ProductImage,
+            };
+		}
+	}
+}
