882da7e7904bdd4e3c0f57e9827a86030513c8d2
uuhuang918 <uuhuang918@gmail.com>
2022-01-13 14:30:55 +0800

    OrderRepository

--- Models/DTOs/CreateOrderItem.cs
+++ Models/DTOs/CreateOrderItem.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NTUB.BookStore.Site.Models.EFModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -12,5 +13,21 @@ namespace NTUB.BookStore.Site.Models.DTOs
 		public int Price{ get; set; }
 		public int  Qty { get; set; }
 		public int SubTotal => Price*Qty;
+	}
+
+
+	public static partial class CreateOrderItemExts
+	{
+		public static OrderItem ToEF(this CreateOrderItem source)
+		{
+			return new OrderItem
+			{
+				ProductId = source.ProductId,
+				ProductName = source.ProductName,
+				Price = source.Price,
+				Qty = source.Qty,
+				SubTotal = source.SubTotal,
+			};
+		}
 	}
 }
---
+++ Models/Infrastructures/Repositories/OrderRepository.cs
@@ -0,0 +1,58 @@
+﻿using NTUB.BookStore.Site.Models.Core.Interfaces;
+using NTUB.BookStore.Site.Models.DTOs;
+using NTUB.BookStore.Site.Models.EFModels;
+using NTUB.BookStore.Site.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NTUB.BookStore.Site.Models.Infrastructures.Repositories
+{
+	public class OrderRepository : IOrderRepository
+	{
+	
+			private readonly AppDbContext _db;
+
+			public OrderRepository(AppDbContext db)
+			{
+				_db = db;
+			}
+
+
+
+			public int Create(CreateOrderRequest request)
+		{
+			var order = new Order
+			{
+				MemberId = request.CustomerId,
+				Total = request.Items.Sum(x => x.SubTotal),
+				CreatedTime = DateTime.Now,
+				Status = 1,
+				Receiver = request.ShippingInfo.Receiver,
+				Address = request.ShippingInfo.Address,
+				CellPhone = request.ShippingInfo.CellPhone,
+				OrderItems = request.Items.Select(x => x.ToEF()).ToList(),
+			};
+			_db.Orders.Add(order);
+			_db.SaveChanges();
+
+			return order.Id;
+		}
+
+		public OrderEntity Load(int orderId)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void RefundByCustomer(int orderId)
+		{
+			throw new NotImplementedException();
+		}
+
+		public IEnumerable<OrderEntity> Search(string customerAccount, DateTime? startTime, DateTime? endTime)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
